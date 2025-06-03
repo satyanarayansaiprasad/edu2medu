8287f6a8725227157c82603849af2495652208c9
Debabrat Swain <swaindebabratt@gmail.com>
2025-06-03 12:58:14 +0530

    update

--- frontend/src/components/MedicalCl.jsx
+++ frontend/src/components/MedicalCl.jsx
@@ -77,7 +77,7 @@ function MedicalCl() {
   );
 
   const renderCarousel = (title, users) => (
-    <div key={title} className="bg-gray-50 py-12 mt-50 ">
+    <div key={title} className="mt-20 sm:mt-24 md:mt-28 lg:mt-30 xl:mt-32 ">
       <header className="mb-6 px-6 md:px-16">
         <h1 className="text-3xl font-extrabold text-gray-900 border-l-4 border-blue-500 pl-4">{title}</h1>
       </header>
